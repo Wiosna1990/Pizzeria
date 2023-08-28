55fb3a7f394462922d62d7d79ef79ebffc867805
Wiosna1990 <zanzarah@op.pl>
2023-08-28 18:28:57 +0200

    Virtual Methods/Override

--- Program.cs
+++ Program.cs
@@ -1,11 +1,24 @@
-﻿var cheddar = new Cheddar();
-var ing = new Ingredient();
+﻿//var cheddar = new Cheddar();
+//var ing = new Ingredient();
+//var pizza = new Pizza();
+//pizza.AddIngredients(new Cheddar());
+//Console.WriteLine(pizza.Describe());
+//Ingredient ingredient = new Cheddar();
+//Console.WriteLine(ingredient.Name);
+var ingredients = new List<Ingredient>
+{
+    new Cheddar(),
+    new Mozzarella(),
+    new TomatoSauce()
+
+};
+
+foreach(var ing in ingredients)
+{
+    Console.WriteLine(ing.Name);
+}
 
 
-Console.WriteLine(cheddar.PublicField);
-Console.WriteLine(ing.PublicField);
-cheddar.PublicField = 16;
-Console.WriteLine(cheddar.PublicField);
 public class Pizza
 {
     private List<Ingredient> _ingredients = new List<Ingredient>();
@@ -14,14 +27,15 @@ public class Pizza
 }
 public class Ingredient
 {
-    public int PublicField = 15;
+    public virtual string Name => "Some of ingredient";
+    public int PublicField;
     public string PublicMethod() => "This method is public in Ingredients class";
     private string PrivateMethod() => "This method is private in Ingredients class";
     protected string ProtectedMethod() => "This method is prothected in Ingredients class";
 }
 public class Cheddar : Ingredient
 {
-    public string Name => "Cheddar cheese";
+    public override string Name => "Cheddar cheese";
     public int AgedInMonths { get; }
     public void UseMethodFromBaseClass()
     {
@@ -37,7 +51,7 @@ public class TomatoSauce : Ingredient
 }
 public class Mozzarella : Ingredient
 {
-    public string Name => "Mozzarella";
+    public override string Name => "Mozzarella";
     public bool IsLight { get; }
 
 }
