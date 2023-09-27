650d4341d4b24df3c9bfa69ea8f8796d97ec4bb0
Wiosna1990 <zanzarah@op.pl>
2023-09-27 12:15:00 +0200

    Few changes

--- Program.cs
+++ Program.cs
@@ -1,26 +1,27 @@
-﻿var pizza = new Pizza();
-pizza.AddIngredient(new Cheddar());
+﻿var pizza = new Pizza(); //new object pizza class
+pizza.AddIngredient(new Cheddar()); // object pizza. function add ingredient . (new object)
 pizza.AddIngredient(new Mozzarella());
 pizza.AddIngredient(new TomatoSauce());
 
-Console.WriteLine(pizza.Describe());
-
-
+Console.WriteLine(pizza.Describe()); // write object pizza. function Describe
 
 
 public class Pizza
 {
-    private List<Ingredient> _ingredients = new List<Ingredient>();
-    public void AddIngredient(Ingredient ingredient) => _ingredients.Add(ingredient);
-    public string Describe() => $"This is a pizza with {string.Join(", ", _ingredients)}";
+    private List<Ingredient> _ingredients = new List<Ingredient>(); // private list _ingredient of class Ingredient = new object
+    public void AddIngredient(Ingredient ingredient) => _ingredients.Add(ingredient); //public void method is Adding Ingredient (object ingrdient)
+    // => object Ingredient. methid Add (ingredient)
+    public string Describe() => $"This is a pizza with {string.Join(", ", _ingredients)}"; //public string method Describe() =>
+    // is adding "," to list
 }
 public class Ingredient
 {
-    public virtual string Name => "Some of ingredient";
+    public virtual string Name => "Some of ingredient"; // Properties string Name return string
     public int PublicField;
-    public string PublicMethod() => "This method is public in Ingredients class";
-    private string PrivateMethod() => "This method is private in Ingredients class";
-    protected string ProtectedMethod() => "This method is protected in Ingredients class";
+    public string PublicMethod() => "This method is public in Ingredients class"; // public Method return string
+    private string PrivateMethod() => "This method is private in Ingredients class"; // private Method return string
+    protected string ProtectedMethod() => "This method is protected in Ingredients class"; // protected Method return string can only
+    // be invoked in base and derrived class
 }
 public class Cheese : Ingredient
 {
@@ -28,9 +29,9 @@ public class Cheese : Ingredient
 }
 public class Cheddar : Cheese
 {
-    public override string Name => "Cheddar cheese";
-    public int AgedInMonths { get; }
-    public void UseMethodFromBaseClass()
+    public override string Name => "Cheddar cheese"; // override properties return string
+    public int AgedInMonths { get; } // getter method
+    public void UseMethodFromBaseClass() // public method return PublicMethod and ProtectedMethod
     {
         
         Console.WriteLine(PublicMethod());
@@ -39,12 +40,12 @@ public class Cheddar : Cheese
 }
 public class TomatoSauce : Ingredient
 {
-    public override string Name => "Tomato sauce";
-    public int TomatosIn100Grams { get; }
+    public override string Name => "Tomato sauce"; // override Properties
+    public int TomatosIn100Grams { get; } //getter
 }
 public class Mozzarella : Cheese
 {
-    public override string Name => "Mozzarella";
-    public bool IsLight { get; }
+    public override string Name => "Mozzarella"; //override Properties
+    public bool IsLight { get; } // public bool getter
 
 }
